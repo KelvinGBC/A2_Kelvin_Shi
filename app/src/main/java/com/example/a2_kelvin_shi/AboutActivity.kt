5bbbb7fb06dd72e2cb0acacd888c414e96a76a18
Kelvin <Kelvin.1122.shi@gmail.com>
2025-12-04 04:42:36 -0500

    Add AboutActivity

---
+++ app/src/main/java/com/example/a2_kelvin_shi/AboutActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a2_kelvin_shi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a2_kelvin_shi.ui.theme.A2_Kelvin_ShiTheme
+
+class AboutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            A2_Kelvin_ShiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    A2_Kelvin_ShiTheme {
+        Greeting2("Android")
+    }
+}
